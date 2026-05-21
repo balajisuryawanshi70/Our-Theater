07222ff88415bffbd249f3ac19aafaa583ab469b
balajisuryawanshi70 <surywanshib7@gmail.com>
2026-05-21 10:30:40 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import SeatLayout from './pages/SeatLayout'
+import Footer from './components/Footer'
+import { Toaster } from 'react-hot-toast'
+
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListBookings from './pages/admin/ListBookings'
+import ListShows from './pages/admin/ListShows'
+
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorites' element={<Favorite />} />
+
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
+
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+export default App
